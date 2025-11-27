b1a15efea8a5016a8331a5cac8a380bc002298fd
mohdhaniff <mohdhaniff@KISWIRE.MY>
2025-11-27 09:11:41 +0800

    change ip

--- localdbConfig.js
+++ localdbConfig.js
@@ -1,7 +1,7 @@
 const local = {
     user: "sa",
     password: "SEAHQAdm!n2019",
-    server: "194.1.31.76",
+    server: "10.21.99.58",
     database: "SFC_WR_DB",
     options: {
       encrypt: false,
